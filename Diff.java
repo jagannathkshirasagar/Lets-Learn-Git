4e4c7fabd42175011a7ac19dba0e533cc3ddbd8a
jagannathkshirasagar <jagandhruva@gmail.com>
2025-03-18 17:46:31 +0530

    added finding of even num btw 1-10

--- Diff.java
+++ Diff.java
@@ -2,6 +2,7 @@ public class Diff {
     public static void main(String[] args) {
         int a=22;
         int b=2;
+        int c=30;
         System.out.println("The difference between the two integeres="+(a-b));
     }
 }
---
+++ EvenNum.java
@@ -0,0 +1,13 @@
+
+    public class EvenNum {
+        public static void main(String[] args) {
+            System.out.println("Even numbers between 1 and 10 are:");
+            
+            for (int i = 1; i <= 10; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
