f21714846d05095e6df6ccc251afb1368534f63f
jagannathkshirasagar <jagandhruva@gmail.com>
2025-03-18 16:45:55 +0530

    first java program sum of two integeres

---
+++ BasicJava.java
@@ -0,0 +1,9 @@
+
+
+public class BasicJava {
+    public static void main(String[] args) {
+       int a=102;
+       int b=51;
+       System.out.println("The sum of teo numbers ="+(a+b));
+    }
+}
---
+++ Diff.java
@@ -0,0 +1,7 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a=22;
+        int b=2;
+        System.out.println("The difference between the two integeres="+(a-b));
+    }
+}
